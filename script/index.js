50c63744860c49e8d062052ff6e624909d87d4c5
abdulkader33447 <abdulkader33447@gmail.com>
2025-03-12 11:53:56 +0600

    testig

--- script/index.js
+++ script/index.js
@@ -1,10 +1,21 @@
 function categoryContainer() {
+  // 1 fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  // 2 convert promise to json
     .then((res) => res.json())
+    //  3 send data to display
     .then((data) => displayCategories(data.categories));
 }
 function displayCategories(categories){
+  //  get the container
+  const categoryContainer=document.getElementById('category-container');
+  // loop operation on Array of object
+  for(let cat of categories){
+    console.log(cat)
+  }
   console.log(categories)
 }
 
 categoryContainer();
+
+console.log('hello');
